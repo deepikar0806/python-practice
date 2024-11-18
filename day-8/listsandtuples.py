2fddbc383792a1fce440226fe8d98fede41a4d42
deepikar0806 <deepika.r0895@gmail.com>
2024-11-18 10:53:15 +0000

    day-8

---
+++ day-8/listsandtuples.py
@@ -0,0 +1,34 @@
+# single string assigned to variable is a variable string
+
+#name = "deepika"
+#print(name)
+
+# what if the 100's of variables needs to be stored then we have the "sequence" (the sequence datatype)
+# we call that as lists
+
+vinay= ["hi","hello","ola"]
+print(vinay)     # real time use of lists comes with getting the list of Ec2 instance , s3 bucket, EBS etc...
+# lists are mutable something that can be changed or resized
+
+deepika = ("vanakam","hello","ola","hi")
+print(deepika)
+
+#print(type(vinay))
+#print(type(deepika))
+
+#vinay.append("namaste")
+#print(vinay)
+
+#vinay.remove("ola")
+#print(vinay)
+
+#deepika.append("namaste")
+#print(deepika)
+
+#new = vinay [1:3]  
+#print (new)
+
+#sorting
+#numbers= [10,2,13,4]
+#numbers.sort()
+#print(numbers)           
